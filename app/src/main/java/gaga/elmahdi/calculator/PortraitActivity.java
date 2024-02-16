d00b73a3cd8ab560875dbbb77536a84fba081b6f
El Mahdi Gaga <elmahdigaga9@gmail.com>
2024-02-16 20:19:20 +0100

    Created Portrait mode activity

---
+++ app/src/main/java/gaga/elmahdi/calculator/PortraitActivity.java
@@ -0,0 +1,14 @@
+package gaga.elmahdi.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PortraitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portrait);
+    }
+}
