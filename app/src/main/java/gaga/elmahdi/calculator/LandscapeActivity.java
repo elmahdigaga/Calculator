d40319855855f085697e6fc4a2f25208595e71f7
El Mahdi Gaga <elmahdigaga9@gmail.com>
2024-02-16 20:56:31 +0100

    Created Landscape mode activity

---
+++ app/src/main/java/gaga/elmahdi/calculator/LandscapeActivity.java
@@ -0,0 +1,14 @@
+package gaga.elmahdi.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandscapeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landscape);
+    }
+}
