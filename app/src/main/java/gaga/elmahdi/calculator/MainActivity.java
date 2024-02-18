59acfe04e06ff4b02eb1f7c358991ef453c5bfff
El Mahdi Gaga <elmahdigaga9@gmail.com>
2024-02-18 19:07:43 +0100

    Added layout switching logic on orientation change

--- app/src/main/java/gaga/elmahdi/calculator/MainActivity.java
+++ app/src/main/java/gaga/elmahdi/calculator/MainActivity.java
@@ -1,14 +1,32 @@
 package gaga.elmahdi.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.activity_portrait);
+        } else {
+            setContentView(R.layout.activity_landscape);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            startActivity(new Intent(this, PortraitActivity.class));
+        } else {
+            startActivity(new Intent(this, LandscapeActivity.class));
+        }
     }
 }
