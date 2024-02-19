535bd16ebdfc3d6298dfbdb1a5c0a3ac8341fc12
El Mahdi Gaga <elmahdigaga9@gmail.com>
2024-02-19 22:33:53 +0100

    Added Initial UI parsing

--- app/src/main/java/gaga/elmahdi/calculator/PortraitActivity.java
+++ app/src/main/java/gaga/elmahdi/calculator/PortraitActivity.java
@@ -8,11 +8,13 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PortraitActivity extends AppCompatActivity implements View.OnClickListener {
+    StringBuilder expression = new StringBuilder();
+    boolean isBracketOpen = false;
     TextView historyTV, resultTV;
-    Button clearAllMB, clearMB, bracketsMB, dotMB, divisionMB, multiplicationMB, additionMB, substractionMB, moduloMB, equalMB;
-    Button zeroMB, oneMB, twoMB, threeMB, fourMB, fiveMB, sixMB, sevenMB, eightMB, nineMB;
+    Button clearAllBtn, clearBtn, bracketsBtn, dotBtn, divisionBtn, multiplicationBtn, additionBtn, substractionBtn, moduloBtn, equalBtn;
+    Button zeroBtn, oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn;
 
-    private Button assignIdToMB(int id) {
+    private Button assignIdToBtn(int id) {
         Button button = findViewById(id);
         button.setOnClickListener(this);
         return button;
@@ -22,28 +24,29 @@ public class PortraitActivity extends AppCompatActivity implements View.OnClickL
         historyTV = findViewById(R.id.historyTextView);
         resultTV = findViewById(R.id.resultTextView);
 
-        clearAllMB = assignIdToMB(R.id.clearAllButton);
-        clearMB = assignIdToMB(R.id.clearButton);
+        clearAllBtn = assignIdToBtn(R.id.clearAllButton);
+        clearBtn = assignIdToBtn(R.id.clearButton);
 
-        bracketsMB = assignIdToMB(R.id.bracketsButton);
-        dotMB = assignIdToMB(R.id.dotButton);
-        divisionMB = assignIdToMB(R.id.divisionButton);
-        multiplicationMB = assignIdToMB(R.id.multplicationButton);
-        additionMB = assignIdToMB(R.id.additionButton);
-        substractionMB = assignIdToMB(R.id.substractionButton);
-        moduloMB = assignIdToMB(R.id.moduloButton);
-        equalMB = assignIdToMB(R.id.equalButton);
+        bracketsBtn = assignIdToBtn(R.id.bracketsButton);
+        dotBtn = assignIdToBtn(R.id.dotButton);
 
-        zeroMB = assignIdToMB(R.id.zeroButton);
-        oneMB = assignIdToMB(R.id.oneButton);
-        twoMB = assignIdToMB(R.id.twoButton);
-        threeMB = assignIdToMB(R.id.threeButton);
-        fourMB = assignIdToMB(R.id.fourButton);
-        fiveMB = assignIdToMB(R.id.fiveButton);
-        sixMB = assignIdToMB(R.id.sixButton);
-        sevenMB = assignIdToMB(R.id.sevenButton);
-        eightMB = assignIdToMB(R.id.eightButton);
-        nineMB = assignIdToMB(R.id.nineButton);
+        divisionBtn = assignIdToBtn(R.id.divisionButton);
+        multiplicationBtn = assignIdToBtn(R.id.multplicationButton);
+        additionBtn = assignIdToBtn(R.id.additionButton);
+        substractionBtn = assignIdToBtn(R.id.substractionButton);
+        moduloBtn = assignIdToBtn(R.id.moduloButton);
+        equalBtn = assignIdToBtn(R.id.equalButton);
+
+        zeroBtn = assignIdToBtn(R.id.zeroButton);
+        oneBtn = assignIdToBtn(R.id.oneButton);
+        twoBtn = assignIdToBtn(R.id.twoButton);
+        threeBtn = assignIdToBtn(R.id.threeButton);
+        fourBtn = assignIdToBtn(R.id.fourButton);
+        fiveBtn = assignIdToBtn(R.id.fiveButton);
+        sixBtn = assignIdToBtn(R.id.sixButton);
+        sevenBtn = assignIdToBtn(R.id.sevenButton);
+        eightBtn = assignIdToBtn(R.id.eightButton);
+        nineBtn = assignIdToBtn(R.id.nineButton);
     }
 
     @Override
@@ -53,11 +56,92 @@ public class PortraitActivity extends AppCompatActivity implements View.OnClickL
         assignIds();
     }
 
+    private void clearHandler(final String token) {
+        if (token.equals("C")) {
+            expression.deleteCharAt(expression.length() - 1);
+        } else {
+            expression.setLength(0);
+        }
+        resultTV.setText("0");
+    }
+
+    private void bracketsHandler() {
+        if (isBracketOpen) {
+            expression.append(")");
+        } else {
+            expression.append("(");
+        }
+        isBracketOpen = !isBracketOpen;
+    }
+
+    private void dotHandler() {
+        if (expression.length() == 0) return;
+        final char lastToken = expression.charAt(expression.length() - 1);
+        if (Character.isDigit(lastToken)) {
+            expression.append(".");
+        }
+    }
+
+    private void operatorHandler(final String op) {
+        if (expression.length() == 0) {
+            if (op.equals("-")) expression.append(op);
+            return;
+        }
+        final char lastToken = expression.charAt(expression.length() - 1);
+        if (Character.isDigit(lastToken) || lastToken == ')') {
+            expression.append(op);
+        }
+    }
+
+    private void equalHandler() {
+        if (expression.length() == 0) return;
+        final char lastToken = expression.charAt(expression.length() - 1);
+        if (!Character.isDigit(lastToken) && lastToken != ')') {
+            return;
+        }
+        resultTV.setText("Total");
+    }
+
     @Override
     public void onClick(View view) {
         Button button = (Button) view;
         String buttonText = button.getText().toString();
-        String newExpression = historyTV.getText().toString() + buttonText;
-        historyTV.setText(newExpression);
+
+        switch (buttonText) {
+            case "C":
+            case "AC":
+                clearHandler(buttonText);
+                break;
+            case "()":
+                bracketsHandler();
+                break;
+            case ".":
+                dotHandler();
+                break;
+            case "+":
+            case "-":
+            case "x":
+            case "/":
+            case "%":
+                operatorHandler(buttonText);
+                break;
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                expression.append(buttonText);
+                break;
+            case "=":
+                equalHandler();
+                break;
+        }
+
+        historyTV.setText(expression.toString());
     }
 }
