055f2f4948cd8c8b0856423a82b12b0e44c90db7
El Mahdi Gaga <elmahdigaga9@gmail.com>
2024-02-20 22:50:50 +0100

    Added scientific function's initial ui handlers

--- app/src/main/java/gaga/elmahdi/calculator/LandscapeActivity.java
+++ app/src/main/java/gaga/elmahdi/calculator/LandscapeActivity.java
@@ -8,10 +8,12 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LandscapeActivity extends AppCompatActivity implements View.OnClickListener {
+    StringBuilder expression = new StringBuilder();
+    boolean isBracketOpen = false;
     TextView historyTV, resultTV;
     Button clearAllBtn, clearBtn, bracketsBtn, dotBtn, divisionBtn, multiplicationBtn, additionBtn, substractionBtn, moduloBtn, equalBtn;
-    Button absoluteValueBtn, piBtn, sqrtBtn, sinBtn, cosBtn, tanBtn, lnBtn, logBtn, invMultBtn, expBtn, squareBtn, powerBtn;
     Button zeroBtn, oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn;
+    Button absoluteValueBtn, piBtn, sqrtBtn, sinBtn, cosBtn, tanBtn, lnBtn, logBtn, invMultBtn, expBtn, squareBtn, powBtn;
 
     private Button assignIdToBtn(int id) {
         Button button = findViewById(id);
@@ -45,7 +47,7 @@ public class LandscapeActivity extends AppCompatActivity implements View.OnClick
         invMultBtn = assignIdToBtn(R.id.inverseMultiplicationButton);
         expBtn = assignIdToBtn(R.id.exponentialButton);
         squareBtn = assignIdToBtn(R.id.squareButton);
-        powerBtn = assignIdToBtn(R.id.powerButton);
+        powBtn = assignIdToBtn(R.id.powButton);
 
         zeroBtn = assignIdToBtn(R.id.zeroButton);
         oneBtn = assignIdToBtn(R.id.oneButton);
@@ -66,11 +68,178 @@ public class LandscapeActivity extends AppCompatActivity implements View.OnClick
         assignIds();
     }
 
+    private void clearHandler(final String token) {
+        if (token.equals("C")) {
+            expression.deleteCharAt(expression.length() - 1);
+        } else {
+            expression.setLength(0);
+        }
+        resultTV.setText("0");
+    }
+
+    private void bracketsHandler() {
+        if (isBracketOpen) {
+            expression.append(")");
+        } else {
+            expression.append("(");
+        }
+        isBracketOpen = !isBracketOpen;
+    }
+
+    private void dotHandler() {
+        if (expression.length() == 0) return;
+        final char lastToken = expression.charAt(expression.length() - 1);
+        if (Character.isDigit(lastToken)) {
+            expression.append(".");
+        }
+    }
+
+    private void operatorHandler(final String op) {
+        if (expression.length() == 0) {
+            if (op.equals("-")) expression.append(op);
+            return;
+        }
+        final char lastToken = expression.charAt(expression.length() - 1);
+        if (Character.isDigit(lastToken) || lastToken == ')') {
+            expression.append(op);
+        }
+    }
+
+    private void equalHandler() {
+        if (expression.length() == 0) return;
+        final char lastToken = expression.charAt(expression.length() - 1);
+        if (!Character.isDigit(lastToken) && lastToken != ')') {
+            return;
+        }
+        resultTV.setText("Total");
+    }
+
+    private void absHandler() {
+
+    }
+
+    private void piHandler() {
+
+    }
+
+    private void sqrtHandler() {
+
+    }
+
+    private void sinHandler() {
+
+    }
+
+    private void cosHandler() {
+
+    }
+
+    private void tanHandler() {
+
+    }
+
+    private void lnHandler() {
+
+    }
+
+    private void logHandler() {
+
+    }
+
+    private void invMulHandler() {
+
+    }
+
+    private void expHandler() {
+
+    }
+
+    private void squareHandler() {
+
+    }
+
+    private void powHandler() {
+
+    }
+
     @Override
     public void onClick(View view) {
         Button button = (Button) view;
         String buttonText = button.getText().toString();
-        String newExpression = historyTV.getText().toString() + buttonText;
-        historyTV.setText(newExpression);
+
+        switch (buttonText) {
+            case "C":
+            case "AC":
+                clearHandler(buttonText);
+                break;
+            case "()":
+                bracketsHandler();
+                break;
+            case ".":
+                dotHandler();
+                break;
+            case "+":
+            case "-":
+            case "x":
+            case "/":
+            case "%":
+                operatorHandler(buttonText);
+                break;
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                expression.append(buttonText);
+                break;
+            case "=":
+                equalHandler();
+                break;
+            case "|x|":
+                absHandler();
+                break;
+            case "π":
+                piHandler();
+                break;
+            case "√":
+                sqrtHandler();
+                break;
+            case "sin":
+                sinHandler();
+                break;
+            case "cos":
+                cosHandler();
+                break;
+            case "tan":
+                tanHandler();
+                break;
+            case "ln":
+                lnHandler();
+                break;
+            case "log":
+                logHandler();
+                break;
+            case "1/x":
+                invMulHandler();
+                break;
+            case "exp":
+                expHandler();
+                break;
+            case "x^2":
+                squareHandler();
+                break;
+            case "x^y":
+                powHandler();
+                break;
+            default:
+                System.out.println("Error: Invalid Token");
+        }
+
+        historyTV.setText(expression.toString());
     }
 }
