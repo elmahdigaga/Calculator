bb1d61927f0899158a62ffcd7f39b0aa10b9db37
El Mahdi Gaga <elmahdigaga9@gmail.com>
2024-02-21 08:52:35 +0100

    Updated activity starting logic based on orientation

--- app/src/main/java/gaga/elmahdi/calculator/MainActivity.java
+++ app/src/main/java/gaga/elmahdi/calculator/MainActivity.java
@@ -7,33 +7,23 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String KEY_CURRENT_ORIENTATION = "current_orientation";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         int orientation = getResources().getConfiguration().orientation;
-        if (savedInstanceState != null) {
-            orientation = savedInstanceState.getInt(KEY_CURRENT_ORIENTATION, orientation);
-        }
-
         launchActivity(orientation);
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        outState.putInt(KEY_CURRENT_ORIENTATION, getResources().getConfiguration().orientation);
-    }
-
     private void launchActivity(final int orientation) {
+        Intent intent;
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            startActivity(new Intent(this, PortraitActivity.class));
+            intent = new Intent(this, PortraitActivity.class);
         } else {
-            startActivity(new Intent(this, LandscapeActivity.class));
+            intent = new Intent(this, LandscapeActivity.class);
         }
+        startActivity(intent);
         finish();
     }
 }
